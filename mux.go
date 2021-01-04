887982b35a754a4ff6a1f46b27d4b2abaaf5ac4e
William <36288420+w8kerr@users.noreply.github.com>
2021-01-04 02:48:12 -0500

    Disable stickies

--- mux.go
+++ mux.go
@@ -58,8 +58,8 @@ func init() {
 		Router.Route("tedit", "Provide translation for the nth tweet (counting upwards) in a Twitter feed channel", Router.TweetEdit)
 		Router.Route("tl", "Translate from Japanese to English", Router.Translate)
 		// Router.Route("extractmessages", "Delete an entire segment of chat messages, in between two messages that match a given pattern", Router.ExtractMessages)
-		Router.Route("sticky", "Make a message stay at the bottom of the chat", Router.Sticky)
-		Router.Route("unsticky", "Stop promoting the sticky in the current channel", Router.Unsticky)
+		// Router.Route("sticky", "Make a message stay at the bottom of the chat", Router.Sticky)
+		// Router.Route("unsticky", "Stop promoting the sticky in the current channel", Router.Unsticky)
 	}
 	// Commands for both remote and dev
 
